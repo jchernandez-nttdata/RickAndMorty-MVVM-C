44ddba03a8c08abcb92668cbe80436694741ab61
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2024-09-13 10:42:26 -0500

    Initial Commit

---
+++ RickAndMorty-MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-MVVM-C
+//
+//  Created by Juan Carlos Hernandez Castillo on 13/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
